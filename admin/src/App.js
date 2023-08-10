03ac8cd92eac3b1f566c10f138413a9ecd2411ac
matthew <larochelle.matthew@gmail.com>
2023-08-10 11:06:08 -0700

    add dynamic new page

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { teacherColumns, horseColumns, studentColumns } from './datatablesource';
 import List from './pages/list/List'
 import Single from './pages/single/Single'
+import New from './pages/new/New';
 
 function App() {
   return (
@@ -12,10 +13,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/teachers" element={<List columns={teacherColumns} />} />
           <Route path="/teachers/:id" element={<Single />} />
+          <Route path="/teachers/new" element={<New />} />
           <Route path="/horses" element={<List columns={horseColumns} />} />
           <Route path="/horses/:id" element={<Single />} />
+          <Route path="/horses/new" element={<New />} />
           <Route path="/students" element={<List columns={studentColumns} />} />
           <Route path="/students/:id" element={<Single />} />
+          <Route path="/students/new" element={<New />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ admin/src/pages/new/New.jsx
@@ -0,0 +1,16 @@
+import './new.scss'
+
+const New = () => {
+    return (
+        <div>
+            New page
+            Ok get the URL parameter and then do a ternary operator or something
+            Form:
+            Horse: Name, Description
+            Teacher: Name, Description
+            Student: fName, lName, age, phone, email, firstLesson
+        </div>
+    )
+}
+
+export default New
