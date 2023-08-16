8752e8fe17303015ca85c9e370fa564b29f2b7b7
matthew <larochelle.matthew@gmail.com>
2023-08-16 13:51:46 -0700

    booking elements are working

--- client/src/pages/booking/Booking.jsx
+++ client/src/pages/booking/Booking.jsx
@@ -1,20 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './booking.scss'
 import { Calendar } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import TimePicker from 'react-time-picker'
-import 'react-time-picker/dist/TimePicker.css';
-import 'react-clock/dist/Clock.css';
+import format from 'date-fns/format'
+import axios from 'axios'
 
 const Booking = () => {
     const [lessonDate, setLessonDate] = useState('')
-    const [value, setValue] = useState('12:00')
+    const [lessonTime, setLessonTime] = useState('')
+    const [teacher, setTeacher] = useState('')
+    const [horse, setHorse] = useState('')
+    const [listOfTeachers, setListOfTeachers] = useState([])
+    const [listOfHorses, setListOfHorses] = useState([])
 
-    const handleSelect = (date) => {
-        console.log(date); // native Date object
-        setLessonDate(date)
-        console.log(lessonDate)
+    useEffect(() => {
+        const getTeachers = async () => {
+            try {
+                const teacherData = await axios.get('/teachers')
+                setListOfTeachers(teacherData.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        const getHorses = async () => {
+            try {
+                const horseData = await axios.get('/horses')
+                setListOfHorses(horseData.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getTeachers()
+        getHorses()
+    }, [])
+
+    const check = () => {
+        console.log(lessonDate, lessonTime, teacher, horse)
+    }
+
+    const handleDate = (date) => {
+        setLessonDate(format(date, 'MM/dd/yyyy'))
+    }
+
+    const handleTime = (e) => {
+        const time = e.target.value
+        setLessonTime(time)
+    }
+
+    const handleTeacher = (e) => {
+        const selectedTeacher = e.target.value
+        setTeacher(selectedTeacher)
+    }
+
+    const handleHorse = (e) => {
+        const selectedHorse = e.target.value
+        setHorse(selectedHorse)
     }
 
     // Create a new date object for today (or the start date)
@@ -29,27 +70,51 @@ const Booking = () => {
             <h1>Book a lesson</h1>
 
             <h3>Choose a date</h3>
+            <input value={ lessonDate } readOnly />
             <Calendar
                 date={new Date()}
-                onChange={handleSelect}
+                onChange={handleDate}
                 minDate={today}
                 maxDate={futureDate}
+                color="#3d91ff"
             />
+
             <h3>Choose a time</h3>
-            <TimePicker 
-                onChange={setValue} 
-                value={value}
-                clearIcon="Clear"  
-                format="h a"
-                maxTime="18:00"
-                minTime="11:00"
-                />
-            <p>Time picker next</p>
-            <p>Time picker should be between 11am-5pm - this should be adjustable on the admin side</p>
-            <p>Then pick a teacher between the 2 that exist</p>
-            <p>Teachers should have a limit on how many lessons they can do per day</p>
-            <p>Then pick horses from the ones which are available</p>
-            <p>Horses should have a limit on how many lessons they can do per day</p>
+            <select id="myDropdown" onChange={handleTime}>
+                <option value="11">11:00 AM</option>
+                <option value="12">12:00 PM</option>
+                <option value="1">1:00 PM</option>
+                <option value="2">2:00 PM</option>
+                <option value="3">3:00 PM</option>
+                <option value="4">4:00 PM</option>
+            </select>
+
+            <h3>Choose a teacher</h3>
+            <select id="myDropdown" onChange={handleTeacher}>
+            {listOfTeachers.map((teacher) => (
+                <option 
+                    value={teacher.name}
+                    key={teacher._id}
+                >
+                    {teacher.name}
+                </option>
+
+            ))}
+            </select>
+
+            <h3>Choose a horse</h3>
+            <select id="myDropdown" onChange={handleHorse}>
+            {listOfHorses.map((horse) => (
+                <option 
+                    value={horse.name}
+                    key={horse._id}
+                >
+                    {horse.name}
+                </option>
+
+            ))}
+            </select>
+            <button onClick={check}>Click me!</button>
         </div>
     )
 }
